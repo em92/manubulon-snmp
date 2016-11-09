c03d1ab0b655471326abc219d5368032b37ccc8c
Eugene Molotov <eugene.m92@gmail.com>
2016-11-09 12:59:11 +0500

    fixed -f output for -T polygon

--- plugins/check_snmp_load.pl
+++ plugins/check_snmp_load.pl
@@ -816,9 +816,9 @@ for (my $i=0;$i<3;$i++) {
 }
 print " OK" if ($exit_val eq $ERRORS{"OK"});
 if (defined($o_perf)) {
-  print " | load_1_min=$load[0]%;$o_warnL[0];$o_critL[0] ";
-  print "load_5_min=$load[1]%;$o_warnL[1];$o_critL[1] ";
-  print "load_15_min=$load[2]%;$o_warnL[2];$o_critL[2]\n";
+  print " | load_5_sec=$load[0]%;$o_warnL[0];$o_critL[0] ";
+  print "load_1_min=$load[1]%;$o_warnL[1];$o_critL[1] ";
+  print "load_5_min=$load[2]%;$o_warnL[2];$o_critL[2]\n";
 } else {
   print "\n";
 }
